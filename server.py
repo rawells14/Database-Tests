f439c3450573dbd162743ee326e2ac110a5bbc05
Ryan Wells <ryan@rawells.com>
2015-03-27 09:24:22 -0500

    Added Some general data

--- server.py
+++ server.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
      
 @app.route('/')
 def index():
+    return "Index"
+
+@app.route('/signup', methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        return "You just submitted: " + request.form['username'] + "   " + request.form['password']
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
